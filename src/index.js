7d28150ff19f70c3403aa104b73346abbdecc60e
Gordon L. Hempton <ghempton@gmail.com>
2017-05-28 19:13:39 -0700

    Require babel plugin directly and bump version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import coffeeReactTransform from 'coffee-react-transform';
 import { convert } from 'decaffeinate';
 import { transform } from 'babel-core';
+import createElementTransform from 'babel-plugin-transform-react-createelement-to-jsx';
 import fs from 'fs';
 
 export function run(args) {
@@ -10,7 +11,7 @@ export function run(args) {
   source = coffeeReactTransform(source);
   source = convert(source).code;
   source = transform(source, {
-    plugins: ['transform-react-createelement-to-jsx'],
+    plugins: [createElementTransform],
   }).code;
   console.log(source);
 }
